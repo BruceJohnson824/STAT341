026a89ddc367ca1145ac203a762cb458a2364dfb
BruceJohnson824 <brujohnson@claflin.edu>
2025-08-28 16:19:30 -0400

    Add files via upload

---
+++ Descriptive-Stats.R
@@ -0,0 +1,13 @@
+#Author: Bruce Johnson; Date: Aug 28,205; Purpose: Measures of descriptive stats
+
+#Genterate dummy/sample dataset
+data <- c(1:50)
+
+#Calculate mean/average for data
+mean(data)
+
+#Calulate median for data
+median(data)
+
+#Calculate standard deviation for data
+sd(data)
