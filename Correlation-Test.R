edf0f24aa364a9da0420db31f477dbe2d14f8021
BruceJohnson824 <brujohnson@claflin.edu>
2025-09-04 16:21:39 -0400

    Add files via upload

---
+++ Correlation-Test.R
@@ -0,0 +1,9 @@
+#Author:Bruce Johnson, Date: 09/04/205, Purpose: Test the correlation
+
+if(!require(devtools)) install.packages("devtools"); devtools::install_github("kassambara/ggpubr")
+
+#Load the library ggubr
+library(ggpubr) 
+
+
+
